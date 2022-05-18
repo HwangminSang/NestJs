4b63803ca80e84d16b664c12c35cdf6499a67167
HwangminSang <alstkd1q1q@naver.com>
2022-05-19 01:01:29 +0900

    swagger 문서 제작중

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,44 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
 declare const module: any;
 
 // nest 시작작
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port= process.env.PORT || 5000;
-  await app.listen(5000);
-  console.log(`eqweq${port}`)
 
+
+  //스웨거 설정시 뱐수이름 config로!! 아니면 페이지가 안나온다
+  const config = new DocumentBuilder()
+      .setTitle('HMS NEST_JS API')
+      .setDescription('Nest_Js Api 설계 문서')
+      .setVersion('1.0')
+      .addCookieAuth('connect.sid')
+      .build();
+
+  const document=SwaggerModule.createDocument(app,config);
+  SwaggerModule.setup('api',app,document);
+
+    const port= process.env.PORT || 5001;
+    await app.listen(port,()=>{
+        console.log(`현재 실행되는 포트는 ${port} 입니다`)
+    });
+
+  //쿠기설정
+  app.use(cookieParser());
+  //세션 설정
+  app.use(
+      session({
+        resave : true,
+        saveUninitialized : false ,
+        secret : process.env.COOKIE_SECRET,
+        cookie : {
+          httpOnly : true,
+        }
+      })
+  )
   // hot -reload 적용
   if (module.hot) {
     module.hot.accept();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,27 @@
 import {Body, Controller, Get, Post, Req, Res} from '@nestjs/common';
 import {UsersService} from "./users.service";
 import {JoinRequestDto} from "./dto/join.request.dto";
+import { ApiOkResponse, ApiOperation, ApiResponse, ApiTags, ApiUnauthorizedResponse } from "@nestjs/swagger";
+import { UserResponseDto } from "../common/dto/user.response.dto";
 
+@ApiTags('USER')
 @Controller('api/users')
 export class UsersController {
     //의존성 주입
     constructor(private  readonly  userService:UsersService) {
     }
 
+    @ApiOkResponse({
+        type : UserResponseDto,
+        description : '회원 한명 조회 성공',
+    })
+    @ApiOperation({summary : '내정보 조회'})
     @Get()
-    findAllUser(@Req() req){
+    getUsers(@Req() req){
         return req.user;
     }
 
-    //
+    @ApiOperation({summary : '회원가입'})
     @Post()
     creatUser(@Body() data : JoinRequestDto){
         const {email,nickname,password}= data;
@@ -21,12 +29,22 @@ export class UsersController {
         return  this.userService.creatUser(email,nickname,password);
     }
 
+    // 알아서 STATUS 200으로 설명 및 반환
+    @ApiOkResponse({
+        description : '로그인성공',
+        type : UserResponseDto,
+    })
+    @ApiUnauthorizedResponse({
+        description :'로그인 실패'
+    })
+    @ApiOperation({summary : '로그인'})
     @Post('login')
     logIn():string{
         return  '';
     }
 
     // req , res 웬만하면 쓰지말자 그럼 express에 결합된다!!
+    @ApiOperation({summary : '로그아웃'})
     @Post('logout')
     logOut(@Req() req , @Res() res) {
         req.logOut();
